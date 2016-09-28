5bd5c23cdadacf8ab8931fb1ddab8b372fb5eae6
zabellfour <zabell2814@gmail.com>
2016-09-29 00:14:06 +0300

    Include all JS, add some typogaphy + include carousel

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,20 @@
 import HP from './modules/helpers';
 
-(function ($) {
+(function($) {
 
-  // When DOM is ready
-  $(function () {
-    HP.random(10, 20);
-  });
+    // When DOM is ready
+    $(function() {
+        smoothScroll.init({
+            selector: '[data-scroll]', // Selector for links (must be a class, ID, data attribute, or element tag)
+            selectorHeader: null, // Selector for fixed headers (must be a valid CSS selector) [optional]
+            speed: 500, // Integer. How fast to complete the scroll in milliseconds
+            easing: 'easeInOutCubic', // Easing pattern to use
+            offset: 0, // Integer. How far to offset the scrolling anchor location in pixels
+            callback: function(anchor, toggle) {} // Function to run after scrolling
+        });
+    });
 
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel();
+});
 }(jQuery));
--- src/vendor_entries/vendor.js
+++ src/vendor_entries/vendor.js
@@ -1,3 +1,7 @@
 module.exports = [
-  './bower/jquery/dist/jquery.js'
+  './bower/jquery/dist/jquery.js',
+  './bower/bootstrap-sass/assets/javascripts/bootstrap.min.js',
+  './bower/matchHeight/dist/jquery.matchHeight-min.js',
+  './bower/smooth-scroll/dist/js/smooth-scroll.min.js',
+  './bower/owl.carousel/dist/owl.carousel.min.js'
 ];
