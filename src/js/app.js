f601e8161a0a46478fd0d0a741d10c0788a4bc99
zabellfour <zabell2814@gmail.com>
2016-10-08 01:31:38 +0300

    created all pages + html links

--- src/js/app.js
+++ src/js/app.js
@@ -4,15 +4,12 @@ import HP from './modules/helpers';
 
     // When DOM is ready
     $(function() {
-        smoothScroll.init({
-            selector: '[data-scroll]', // Selector for links (must be a class, ID, data attribute, or element tag)
-            selectorHeader: null, // Selector for fixed headers (must be a valid CSS selector) [optional]
-            speed: 500, // Integer. How fast to complete the scroll in milliseconds
-            easing: 'easeInOutCubic', // Easing pattern to use
-            offset: 0, // Integer. How far to offset the scrolling anchor location in pixels
-            callback: function(anchor, toggle) {} // Function to run after scrolling
-        });
+          smoothScroll.init({
+        selector: '.smooth',
+        offset: 40,
+        speed: 1000
     });
+    }); 
 
     $(document).ready(function() {
         $('.owl-carousel').owlCarousel({
@@ -25,7 +22,7 @@ import HP from './modules/helpers';
     });
 
     $(function() {
-        $('.arr-align').matchHeight(options);
+        $('.arr-align').matchHeight({});
     });
 
 }(jQuery));
